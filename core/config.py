21162050956cd21d2f2213b59d2f1028a84153ab
Razp509 <Razp509@gmail.com>
2024-06-28 19:45:11 +0500

    1.0.0

---
+++ core/config.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+from pathlib import Path
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+class Settings:
+    PROJECT_NAME: str = "FA_Project"
+    PROJECT_VERSION: str = "1.0.0"
+
+    DB_HOST = os.environ.get("DB_HOST", "localhost")
+    DB_PORT = os.environ.get("DB_PORT", 5432)
+    DB_NAME = os.environ.get("DB_NAME")
+    DB_USER = os.environ.get("DB_USER")
+    DB_PASS = os.environ.get("DB_PASS")
+    DB_URL = (f"postgresql://{DB_USER}:"
+        f"{DB_PASS}@{DB_HOST}:"
+        f"{DB_PORT}/{DB_NAME}")
+    
+    
+settings = Settings()
